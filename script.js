27e391b590b6398698d04542d1ad4f585156f979
Jesse Olero <olerojesse@gmail.com>
2025-09-20 11:24:53 +0300

    newfiles

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
     "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "But you're my favorite person!",
+    "Don't make me sad...",
+    "Pleeease?",
+    "You know it's true!",
+    "My bestie!",
+    "Just say yes already!",
+    "I'll ask again!",
+    "You're breaking my heart!",
+    "Okay, one last time... yes?"
 ];
 
 let messageIndex = 0;
@@ -19,9 +19,22 @@ function handleNoClick() {
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+    yesButton.style.fontSize = `${currentSize * 1.2}px`;
 }
 
-function handleYesClick() {
-    window.location.href = "yes_page.html";
+// Slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (slides.length === 0) return;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
